bc4cb9a5791716008aca6b0eadc5308c16be6cdc
Riantsoa11 <tsiory.andrianasoloharison@gmail.com>
2023-10-20 11:58:23 +0200

    Tsiory

--- ANDRIANASOLOHARISON_Tsiory_HANGMAN/MainWindow.xaml.cs
+++ ANDRIANASOLOHARISON_Tsiory_HANGMAN/MainWindow.xaml.cs
@@ -15,14 +15,79 @@ using System.Windows.Shapes;
 
 namespace ANDRIANASOLOHARISON_Tsiory_HANGMAN
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string motATrouver = "Informatique,Bonjour,Vie,"; // Mot à trouver (vous pouvez le changer)
+        private string motCourant = "-------"; // Mot affiché
+
+        private int viesRestantes = 6; // Nombre de vies restantes
+
         public MainWindow()
         {
             InitializeComponent();
+            AfficherMotCourant();
+        }
+
+        private void AfficherMotCourant()
+        {
+         TB_Reponse.Text = motCourant;
+        }
+
+        private void ActualiserVies()
+        {
+            TB_vie.Text = viesRestantes.ToString();
+        }
+
+        private void CheckLettre(char lettre)
+        {
+            bool lettreTrouvee = false;
+
+            for (int i = 0; i < motATrouver.Length; i++)
+            {
+                if (motATrouver[i] == lettre)
+                {
+                    motCourant = motCourant.Substring(0, i) + lettre + motCourant.Substring(i + 1);
+                    lettreTrouvee = true;
+                }
+            }
+
+            if (!lettreTrouvee)
+            {
+                viesRestantes--;
+                ActualiserVies();
+            }
+
+            AfficherMotCourant();
+
+            if (motCourant == motATrouver)
+            {
+                MessageBox.Show("Vous avez gagné !");
+                // Mettez ici le code pour recommencer le jeu.
+            }
+
+            if (viesRestantes == 0)
+            {
+                MessageBox.Show("Vous avez perdu. Le mot était : " + motATrouver);
+                // Mettez ici le code pour recommencer le jeu.
+            }
+        }
+
+        // Les événements de clic pour les boutons A à Z
+
+
+        private void Word_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string letter =  btn.Content.ToString();
+
+        }
+
+        // Répétez ces méthodes pour les autres lettres.
+
+        private void Recommencer_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            
         }
     }
 }

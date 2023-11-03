ec0797d42b0e20ab5170ecd6272c8d12e05f010a
Riantsoa11 <tsiory.andrianasoloharison@gmail.com>
2023-11-03 18:08:03 +0100

    Fonction

--- ANDRIANASOLOHARISON_Tsiory_HANGMAN/MainWindow.xaml.cs
+++ ANDRIANASOLOHARISON_Tsiory_HANGMAN/MainWindow.xaml.cs
@@ -17,20 +17,39 @@ namespace ANDRIANASOLOHARISON_Tsiory_HANGMAN
 {
     public partial class MainWindow : Window
     {
-        private string motATrouver = "Informatique,Bonjour,Vie,"; // Mot à trouver (vous pouvez le changer)
-        private string motCourant = "-------"; // Mot affiché
+        private List<string> listMotATrouver = new List<string>
+        {
+            // Mot à trouver (vous pouvez le changer)
+            "Informatique",
+            "Reseau",
+            "Donnee",
+            "base",
+            "clavier",
+            "souris"
+        };
 
-        private int viesRestantes = 6; // Nombre de vies restantes
+        private char[] motCourant; // Mot affiché
+        private int viesRestantes; // Nombre de vies restantes
+        private string motCache;
+        private int imageVie;
 
         public MainWindow()
         {
             InitializeComponent();
-            AfficherMotCourant();
+            BitmapImage image = new BitmapImage(new Uri("Images/7.png", UriKind.Relative));
+            monImage.Source = image;
+            BoutonStatut(false);
         }
 
         private void AfficherMotCourant()
         {
-         TB_Reponse.Text = motCourant;
+            TB_Reponse.Text = new string(motCourant);
+        }
+
+        private void AfficherImage(int imageVie)
+        {
+            BitmapImage image = new BitmapImage(new Uri("Images/" + imageVie + ".png", UriKind.Relative));
+            monImage.Source = image;
         }
 
         private void ActualiserVies()
@@ -38,56 +57,162 @@ namespace ANDRIANASOLOHARISON_Tsiory_HANGMAN
             TB_vie.Text = viesRestantes.ToString();
         }
 
-        private void CheckLettre(char lettre)
-        {
-            bool lettreTrouvee = false;
-
-            for (int i = 0; i < motATrouver.Length; i++)
-            {
-                if (motATrouver[i] == lettre)
-                {
-                    motCourant = motCourant.Substring(0, i) + lettre + motCourant.Substring(i + 1);
-                    lettreTrouvee = true;
-                }
-            }
-
-            if (!lettreTrouvee)
-            {
-                viesRestantes--;
-                ActualiserVies();
-            }
-
-            AfficherMotCourant();
-
-            if (motCourant == motATrouver)
-            {
-                MessageBox.Show("Vous avez gagné !");
-                // Mettez ici le code pour recommencer le jeu.
-            }
-
-            if (viesRestantes == 0)
-            {
-                MessageBox.Show("Vous avez perdu. Le mot était : " + motATrouver);
-                // Mettez ici le code pour recommencer le jeu.
-            }
-        }
-
-        // Les événements de clic pour les boutons A à Z
-
 
         private void Word_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            string letter =  btn.Content.ToString();
+            string letter = btn.Content.ToString();
+            CheckLettre(char.Parse(letter));
+            btn.IsEnabled = false;
+        }
+
+        private void InitialiserValeur()
+        {
+            viesRestantes = 6; // Nombre de vies restantes
+            motCache = "";
+            imageVie = 1;
+            monImage.Source = null;
+
+
+            // choisir le mot
+            Random random = new Random();
+            int aleatoire = random.Next(0, listMotATrouver.Count);
+            motCache = listMotATrouver[aleatoire];
+
+            ActualiserVies();
+        }
+
+        private void BoutonStatut(bool estActif)
+        {
+            A.IsEnabled = estActif;
+            B.IsEnabled = estActif;
+            C.IsEnabled = estActif;
+            D.IsEnabled = estActif;
+            E.IsEnabled = estActif;
+            F.IsEnabled = estActif;
+            G.IsEnabled = estActif;
+            H.IsEnabled = estActif;
+            I.IsEnabled = estActif;
+            J.IsEnabled = estActif;
+            K.IsEnabled = estActif;
+            L.IsEnabled = estActif;
+            M.IsEnabled = estActif;
+            N.IsEnabled = estActif;
+            O.IsEnabled = estActif;
+            P.IsEnabled = estActif;
+            Q.IsEnabled = estActif;
+            R.IsEnabled = estActif;
+            S.IsEnabled = estActif;
+            T.IsEnabled = estActif;
+            U.IsEnabled = estActif;
+            V.IsEnabled = estActif;
+            W.IsEnabled = estActif;
+            X.IsEnabled = estActif;
+            Y.IsEnabled = estActif;
+            Z.IsEnabled = estActif;
+            TB_Terminer.IsEnabled = estActif;
+            TB_Indice.IsEnabled = estActif;
+        }
+
+        private void Commencer_Click(object sender, RoutedEventArgs e)
+        {
+            InitialiserValeur();
+
+            // Activer le clavier
+            BoutonStatut(true);
+
+
+            Button btn = sender as Button;            
+
+            // cacher le mot
+            motCourant = new char[motCache.Length * 2 - 1];
+
+            for (int i = 0; i < motCache.Length; i++)
+            {
+                motCourant[i*2] = '_';
+
+                if (i < motCache.Length - 1)
+                {
+                    motCourant[i * 2 + 1] = ' ';
+                }
+            };
+            TB_Reponse.Text = new string(motCourant);
+
+            btn.IsEnabled = false;
 
         }
 
-        // Répétez ces méthodes pour les autres lettres.
-
-        private void Recommencer_Click(object sender, RoutedEventArgs e)
+        private void CheckLettre(char lettre) 
         {
-            Button btn = sender as Button;
-            
+            bool lettretrouvee = false;
+            for(int i = 0; i < motCache.Length; i++) 
+            {
+                if (motCache[i] == char.ToLower(lettre) || motCache[i] == char.ToUpper(lettre))
+                {
+                    motCourant[i*2] = lettre;
+                    lettretrouvee = true;
+                }
+            }
+
+            if (!lettretrouvee)
+            {
+                viesRestantes--;
+                ActualiserVies();
+                AfficherImage(imageVie);
+                imageVie++;
+
+            }
+
+            AfficherMotCourant();
+
+            //Manala espace
+            if (new string(motCourant).Replace(" ","") == motCache.ToUpper())
+            {
+                MessageBox.Show("Jeu terminé !");
+                TB_Commencer.IsEnabled = true;
+                BoutonStatut(false);
+            }
+
+            if (viesRestantes == 0)
+            {
+                MessageBox.Show("Vous avez perdu. Le mot était : " + motCache);
+                TB_Commencer.IsEnabled = true;
+                BoutonStatut(false);
+            }
+        }
+
+        private void TB_Terminer_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in motCache.ToUpper())
+            {
+                CheckLettre(item);
+            }
+            BoutonStatut(false);
+        }
+
+        private void TB_Indice_Click(object sender, RoutedEventArgs e)
+        {
+            string motActu = new string(motCourant);
+            motActu.Replace(" ", "");
+            motActu.Replace("_", "");
+
+            List<char> lettresList = new List<char>();
+
+            foreach (char item in motCache.ToUpper())
+            {
+                if (!motActu.Contains(item))
+                {
+                    lettresList.Add(item);
+                }
+            }
+
+            Random random = new Random();
+            int aleatoire = random.Next(0, lettresList.Count);
+            char lettre = lettresList[aleatoire];
+            CheckLettre(lettre);
+            Button boutonCible = FindName(lettre.ToString()) as Button;
+            boutonCible.IsEnabled = false;
+            TB_Indice.IsEnabled = false;
         }
     }
 }
